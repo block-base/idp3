4eaaf002a9757feb38f49e1d5968ab33848d864b
taijusanagi <taijusanagi@gmail.com>
2023-06-09 10:06:46 +0700

    feat: home ui

--- packages/app/src/components/Home/Home.tsx
+++ packages/app/src/components/Home/Home.tsx
@@ -8,13 +8,17 @@ export const Home = () => {
   const router = useRouter();
   return (
     <section>
-      <Button
-        onClick={() => {
-          router.push("./demo");
-        }}
-      >
-        Demo
-      </Button>
+      <div className="bg-black h-screen flex flex-col items-center justify-center text-white">
+        <h1 className="text-5xl mb-5">IdP3</h1>
+        <p className="mb-5 text-xl">Decentralized Identity Aggregator</p>
+        <Button
+          onClick={() => {
+            router.push("./demo");
+          }}
+        >
+          Demo
+        </Button>
+      </div>
     </section>
   );
 };
