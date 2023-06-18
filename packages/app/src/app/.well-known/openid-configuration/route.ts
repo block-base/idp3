3023fd39434e99679055dbe0525c135c6d7443b3
taijusanagi <taijusanagi@gmail.com>
2023-06-18 16:44:27 +0700

    fix: oid config

--- packages/app/src/app/.well-known/openid-configuration/route.ts
+++ packages/app/src/app/.well-known/openid-configuration/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 import openidConfiguration from "../../../config/openid-configuration.json";
 
 export async function GET() {
+  openidConfiguration.authorization_endpoint = `${process.env.NEXT_PUBLIC_APP_URL}/authorize`;
+  openidConfiguration.issuer = `${process.env.NEXT_PUBLIC_APP_URL}`;
   return NextResponse.json(openidConfiguration, {
     status: 200,
     headers: {
